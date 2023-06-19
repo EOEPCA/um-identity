4c863f8d8ba8fd7d3179db99fcdf77d2987d845d
Daniel Pimenta <105205108+daniel-pimenta-DME@users.noreply.github.com>
2023-06-19 17:00:10 +0100

    Eoepca 934 um keycloak change um design to be based on keycloak (#1)

---
+++ tests/test_resources.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestResources(unittest.TestCase):
+
+    def test_insert(self):
+        #self.assertEqual('foo'.upper(), 'FOO')
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
