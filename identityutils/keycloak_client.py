d0ea72b9ba057a0fec2d1a620db2157f7f9f87e0
flaviorosadme <flavio.rosa@deimos.com.pt>
2023-09-29 11:10:10 +0100

    added

--- identityutils/keycloak_client.py
+++ identityutils/keycloak_client.py
@@ -63,6 +63,7 @@ class KeycloakClient:
         _client_id = self.keycloak_admin.get_client_id(client_id)
         logger.info(_client_id)
         client_id = self.resources_client.get("id")
+        logger.info(client_id)
         response = self.keycloak_admin.create_client_authz_resource(client_id=client_id, payload=resource,
                                                                     skip_exists=True)
         logger.info('Created resource:\n' + json.dumps(resource, indent=2))
