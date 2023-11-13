b821e42d71f0a8e722f7cf787a010580437414bb
Daniel Pimenta <daniel.pimenta@deimos.com.pt>
2023-11-13 17:58:00 +0000

    Update create-client.sh

--- create-client.sh
+++ create-client.sh
@@ -1,106 +1,134 @@
 #!/bin/bash
-i=0
-resource_names_list=()
-resource_uris_list=()
-resource_scopes_list=()
-users_list=()
-roles_list=()
+
+usage="$(basename "$0") [-h] [--clientid id] [--clientname name] [--resourcename name] [--resourceuris u1,u2] [--scopes s] [--users u1,u2] [--roles r1,r2] -- add a client with protected resources
+
+where:
+    -h                  show help message
+    --default-resource  add default resource - /* authenticated
+    --clientid          client id
+    --clientname        client name
+    --resourcename      resource name
+    --resourceuris      resource uris - separated by comma (,)
+    --scopes            resource scopes - separated by comma (,)
+    --users             user names with access to the resource - separated by comma (,)
+    --roles             role names with access to the resource - separated by comma (,)
+"
+
+client_id=""
+client_name=""
+resource_name=""
+resource_uris=()
+resource_scopes=()
+users=()
+roles=()
+
+resources=()
 
 while test $# -gt 0; do
            case "$1" in
-                -clientid)
+                --clientid)
                     shift
+                    if [ ! -z "${client_id}" ]; then
+                      add_resource()
+                    fi
                     client_id=$1
                     shift
                     ;;
-                -clientname)
+                --clientname)
                     shift
+                    if [ ! -z "${client_name}" ]; then
+                      add_resource()
+                    fi
                     client_name=$1
                     shift
                     ;;
-                -resourcename) 
+                --resourcename)
                     shift
-                    resource_names_list[$i]=$1
-                    i=$(($i+1))
-                    shift
-                    ;;
-                -resourceuris) 
-                    shift
-                    resource_uris_list[$i]=$1
+                    resource_name=$1
                     shift
                     ;;
-                -scopes) 
+                --default-resource)
                     shift
-                    resource_scopes_list[$i]=$1
+                    resource_uris=("/*")
+                    resource_scopes=()
+                    users=()
+                    roles=()
+                    add_resource()
                     shift
                     ;;
-                -users) 
+                --resourceuris)
                     shift
-                    users_list[$i]=$1
+                    resource_uris=$1
                     shift
                     ;;
-                -roles) 
+                --scopes)
                     shift
-                    roles_list[$i]=$1
+                    resource_scopes=$1
                     shift
+                    ;;
+                --users)
+                    shift
+                    users=$1
+                    shift
+                    ;;
+                --roles)
+                    shift
+                    roles=$1
+                    shift
+                    ;;
+                -h)
+                    echo "$usage"
+                    return 1;
                     ;;
                 *)
-                   echo "$1 is not a recognized flag!"
+                   echo "$usage"
                    return 1;
                    ;;
           esac
-done  
-
-json_array() {
-  echo -n '['
-  while [ $# -gt 0 ]; do
-    x=${1//\\/\\\\}
-    echo -n "\"${x//\"/\\\"}\""
-    [ $# -gt 1 ] && echo -n ', '
-    shift
-  done
-  echo ']'
-}
-
-echo ${resource_names_list[@]}
-echo ${resource_uris_list[@]}
-echo ${users_list[@]}
-echo ${roles_list[@]}
-
-
-resources=()
-for ind in "${!resource_names_list[@]}"; do
-
-  resource_name=${resource_names_list[ind]}
-  resource_uris=${resource_uris_list[ind]}
-  resource_scopes=${resource_scopes_list[ind]}
-  users=${users_list[ind]}
-  roles=${roles_list[ind]}
-
-  if [ -z "${resource_scopes}" ]; then
-    resource_scopes="access"
-  fi
-  IFS=',' read -ra resource_uris_array <<<"$resource_uris"
-  IFS=',' read -ra resource_scopes_array <<<"$resource_scopes"
-  IFS=',' read -ra users_array <<<"$users"
-  IFS=',' read -ra roles_array <<<"$roles"
-  if ((${#users_array[@]} == 0 && ${#roles_array[@]} == 0)); then
-    echo "Resource permission requires at least one user or role... skipping"
-  else
-    resource="{
-            \"resource\": {
-              \"name\": \"${resource_name}\",
-              \"uris\": $(json_array "${resource_uris_array[@]}"),
-              \"resource_scopes\": $(json_array "${resource_scopes_array[@]}")
-            },
-            \"permissions\": {
-              \"user\": $(json_array "${users_array[@]}"),
-              \"role\": $(json_array "${roles_array[@]}")
-            }
-          }"
-        resources+=("$resource")
-  fi
 done
+
+
+if (( ${#resources[@]} == 0 )); then
+  read -rp "Client Id: " client_id
+  read -rp "Client Name: " client_name
+  read -rp "Client Description: " client_description
+  read -rp "Add resource? [y/N] " add_resource
+  resources=()
+  while [ "$add_resource" == y ]; do
+    read -rp "Resource name: " resource_name
+    read -rp "Resource URIs: " resource_uris
+    read -rp "Resource scopes (optional): " resource_scopes
+    if [ -z "${resource_scopes}" ]; then
+      resource_scopes="access"
+    fi
+    read -rp "Users: " users
+    read -rp "Roles: " roles
+    echo "test"
+    IFS=',' read -ra resource_uris_array <<<"$resource_uris"
+    IFS=',' read -ra resource_scopes_array <<<"$resource_scopes"
+    IFS=',' read -ra users_array <<<"$users"
+    IFS=',' read -ra roles_array <<<"$roles"
+    if ((${#users_array[@]} == 0 && ${#roles_array[@]} == 0)); then
+      echo "Resource permission requires at least one user or role... skipping"
+    else
+      resource="{
+              \"resource\": {
+                \"name\": \"${resource_name}\",
+                \"uris\": $(json_array "${resource_uris_array[@]}"),
+                \"resource_scopes\": $(json_array "${resource_scopes_array[@]}")
+              },
+              \"permissions\": {
+                \"user\": $(json_array "${users_array[@]}"),
+                \"role\": $(json_array "${roles_array[@]}")
+              }
+            }"
+          resources+=("$resource")
+    fi
+    read -rp "Add resource? [y/N] " add_resource
+  done
+fi
+
+
 payload=""
 if ((${#resources[@]} == 0)); then
   payload="{
@@ -117,3 +145,40 @@ else
   }"
 fi
 echo $payload
+
+add_resource() {
+  IFS=',' read -ra resource_uris_array <<<"$resource_uris"
+  IFS=',' read -ra resource_scopes_array <<<"$resource_scopes"
+  IFS=',' read -ra users_array <<<"$users"
+  IFS=',' read -ra roles_array <<<"$roles"
+  resource="{
+    \"resource\": {
+      \"name\": \"${resource_name}\",
+      \"uris\": $(json_array "${resource_uris_array[@]}"),
+      \"resource_scopes\": $(json_array "${resource_scopes_array[@]}")
+    },
+    \"permissions\": {
+      \"user\": $(json_array "${users_array[@]}"),
+      \"role\": $(json_array "${roles_array[@]}")
+    }
+  }"
+  resources+=("$resource")
+  client_id=""
+  client_name=""
+  resource_name=""
+  resource_uris=()
+  resource_scopes=()
+  users=()
+  roles=()
+}
+
+json_array() {
+  echo -n '['
+  while [ $# -gt 0 ]; do
+    x=${1//\\/\\\\}
+    echo -n "\"${x//\"/\\\"}\""
+    [ $# -gt 1 ] && echo -n ', '
+    shift
+  done
+  echo ']'
+}
